90632ba7df306d116a3655258077b4fd70bd73a1
Jan Tatham <jan@sebity.com>
2024-03-16 16:19:35 +0000

    Inital Commit

---
+++ Source/ActionPlatformer/ActionPlatformer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionPlatformer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionPlatformer, "ActionPlatformer" );
